e9b142f5679a68069afdf81ddec70177ff9ad340
Mashiatmm <1705005@ugrad.cse.buet.ac.bd>
2023-01-24 01:38:00 +0600

    Regression models added

--- genetic data/tpot_sonar_best_model.py
+++ genetic data/tpot_sonar_best_model.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
 from tpot.builtins import StackingEstimator
+from xgboost import XGBClassifier
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -12,10 +13,10 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=1)
 
-# Average CV score on the training set was: 0.8229710144927537
+# Average CV score on the training set was: 0.7606893004115226
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=GradientBoostingClassifier(learning_rate=0.5, max_depth=8, max_features=0.35000000000000003, min_samples_leaf=6, min_samples_split=18, n_estimators=100, subsample=0.8500000000000001)),
-    RandomForestClassifier(bootstrap=True, criterion="entropy", max_features=0.15000000000000002, min_samples_leaf=15, min_samples_split=16, n_estimators=100)
+    StackingEstimator(estimator=XGBClassifier(learning_rate=0.001, max_depth=7, min_child_weight=15, n_estimators=100, n_jobs=1, subsample=0.7500000000000001, verbosity=0)),
+    LogisticRegression(C=20.0, dual=False, penalty="l2")
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 1)
