bcd75fa53075b7db66c5f0f71571046cfc0fc6a2
Mashiatmm <1705005@ugrad.cse.buet.ac.bd>
2023-01-29 23:17:43 +0600

    seed added

--- genetic data/tpot_sonar_best_model.py
+++ genetic data/tpot_sonar_best_model.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import RobustScaler
 from tpot.builtins import StackingEstimator
-from xgboost import XGBClassifier
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -13,10 +13,11 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=1)
 
-# Average CV score on the training set was: 0.7606893004115226
+# Average CV score on the training set was: 0.7507092198581559
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=XGBClassifier(learning_rate=0.001, max_depth=7, min_child_weight=15, n_estimators=100, n_jobs=1, subsample=0.7500000000000001, verbosity=0)),
-    LogisticRegression(C=20.0, dual=False, penalty="l2")
+    RobustScaler(),
+    StackingEstimator(estimator=KNeighborsClassifier(n_neighbors=31, p=1, weights="uniform")),
+    KNeighborsClassifier(n_neighbors=55, p=2, weights="distance")
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 1)
